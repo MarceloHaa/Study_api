d99961fc5f2115feff8047d73746bcbd0bf87185
MarceloHaa <marcelohenrique.a@hotmail.com>
2024-09-12 17:33:57 -0300

    alterado o footer

--- src/App.js
+++ src/App.js
@@ -13,24 +13,28 @@ import Filmes from './pages/Filmes';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
     return (
         <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/pokemons" element={<Pokemons />} />
-                <Route path="/harrypotter" element={<HarryPotter />} />
-                <Route path="/jogue21" element={<Jogue21 />} />
-                <Route path="/previsoes" element={<Previsao />} />
-                <Route path="/graficos" element={<Graficos />} />
-                <Route path="/conversor" element={<Conversor />} />
-                <Route path="/noticias" element={<Noticias />} />
-                <Route path="/filmes" element={<Filmes />} />
-            </Routes>
-            <Footer />
+            <div className={styles.app}>
+                <Navbar />
+                <main className={styles.mainContent}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/pokemons" element={<Pokemons />} />
+                        <Route path="/harrypotter" element={<HarryPotter />} />
+                        <Route path="/jogue21" element={<Jogue21 />} />
+                        <Route path="/previsoes" element={<Previsao />} />
+                        <Route path="/graficos" element={<Graficos />} />
+                        <Route path="/conversor" element={<Conversor />} />
+                        <Route path="/noticias" element={<Noticias />} />
+                        <Route path="/filmes" element={<Filmes />} />
+                    </Routes>
+                </main>
+                <Footer />
+            </div>
         </Router>
     );
 }
